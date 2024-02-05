75961ecc5a74ff45347cea595f908fb636eba527
jkobb510 <41555063+jkobb510@users.noreply.github.com>
2024-02-05 00:51:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-zah3cx
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/jkobb510/web-platform-zah3cx)
---
+++ script.js
@@ -0,0 +1,5 @@
+console.log('Hello!');
+function renderGif() {
+  document.getElementById('renderButton').style.display = 'none';
+  document.getElementById('gifContainer').style.display = 'block';
+}
